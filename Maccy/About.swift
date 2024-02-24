1de7745602512e68efc598175815fd1de136e59f
Pierre Houston <jpmhouston+github@gmail.com>
2024-02-23 20:05:07 -0800

    About box blurb, addresses, copyright string, version number

--- Maccy/About.swift
+++ Maccy/About.swift
@@ -1,20 +1,14 @@
 import Cocoa
 
 class About {
-//  private let familyCredits = NSAttributedString(
-//    string: "Special thank you to Tonia & Guy! ❤️",
-//    attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor]
-//  )
-//
-//  private var kossCredits: NSMutableAttributedString {
-//    let string = NSMutableAttributedString(string: "Kudos to Sasha Koss for help! 🏂",
-//                                           attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
-//    string.addAttribute(.link, value: "https://koss.nocorp.me", range: NSRange(location: 9, length: 10))
-//    return string
-//  }
+  private var blurb: NSAttributedString {
+    return NSAttributedString(
+      string: "Cleepp adds a new mode to the clipboard\nthat lets you copy multiple times from one\nplace then paste them all in order elsewhere.",
+      attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor]);
+  }
 
-  private var forkAuthorCredits: NSMutableAttributedString {
-      let string = NSMutableAttributedString(string: "Thank you to authors of Maccy",
+  private var forkCredits: NSMutableAttributedString {
+      let string = NSMutableAttributedString(string: "Thank you to authors of Maccy which this\napp is a derivative of. Check it out for a\nfull-featured clipboard history manager.",
                                              attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
       string.addAttribute(.link, value: "https://maccy.app", range: NSRange(location: 24, length: 5))
       return string
@@ -23,20 +17,20 @@ class About {
   private var links: NSMutableAttributedString {
     let string = NSMutableAttributedString(string: "Website│GitHub│Support",
                                            attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
-    string.addAttribute(.link, value: "https://bananameter.lol", range: NSRange(location: 0, length: 7))
-    string.addAttribute(.link, value: "https://github.com/jpmhouston/ClipStack", range: NSRange(location: 8, length: 6))
-    string.addAttribute(.link, value: "mailto:support@bananameter.lol", range: NSRange(location: 15, length: 7))
+    string.addAttribute(.link, value: "https://cleepp.bananameter.lol", range: NSRange(location: 0, length: 7))
+    string.addAttribute(.link, value: "https://github.com/jpmhouston/Cleepp", range: NSRange(location: 8, length: 6))
+    string.addAttribute(.link, value: "mailto:cleepp@bananameter.lol", range: NSRange(location: 15, length: 7))
     return string
   }
 
-  private var credits: NSMutableAttributedString {
+  private var credits: NSAttributedString {
     let credits = NSMutableAttributedString(string: "",
                                             attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
     credits.append(links)
     credits.append(NSAttributedString(string: "\n\n"))
-//    credits.append(kossCredits)
-//    credits.append(NSAttributedString(string: "\n"))
-    credits.append(forkAuthorCredits)
+    credits.append(blurb)
+    credits.append(NSAttributedString(string: "\n\n"))
+    credits.append(forkCredits)
     credits.setAlignment(.center, range: NSRange(location: 0, length: credits.length))
     return credits
   }
