214c349e80db2e3f83db163b0d38cb3f047350a2
Pierre Houston <jpmhouston+github@gmail.com>
2024-03-19 17:21:04 -0700

    Improved about box, qualifier for appstore build, tighten paragraphs, appstore build wrong copyright

--- Maccy/About.swift
+++ Maccy/About.swift
@@ -15,49 +15,66 @@ class About {
   private var blurb: NSAttributedString {
     return NSAttributedString(
       string: "Cleepp adds a new mode to the clipboard\nthat lets you copy multiple times from one\nplace then paste them all in order elsewhere.",
-      attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor]);
+      attributes: [.foregroundColor: NSColor.labelColor]);
   }
   
   private var introLink: NSAttributedString {
     let string = NSMutableAttributedString(string: "More information: Show Intro",
-                                           attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+                                           attributes: [.foregroundColor: NSColor.labelColor])
     string.addAttribute(.link, value: About.showIntroInAppURL, range: NSRange(location: 18, length: 10))
     return string
   }
   
   private var forkCredits: NSMutableAttributedString {
-      let string = NSMutableAttributedString(string: "Thank you to authors of Maccy which this\napp is a derivative of. Check it out for a\nfull-featured clipboard history manager.",
-                                             attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
-    string.addAttribute(.link, value: About.maccyURL, range: NSRange(location: 24, length: 5))
-      return string
+    let string = NSMutableAttributedString(
+      string: "Thank you to the authors of Maccy which this app is a derivative of. Check it out here for a more full-featured clipboard history manager.",
+      attributes: [.foregroundColor: NSColor.labelColor])
+    string.addAttribute(.link, value: About.maccyURL, range: NSRange(location: 82, length: 4))
+    return string
   }
   
   private var links: NSAttributedString {
     let string = NSMutableAttributedString(string: "Website│GitHub│Support",
-                                           attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+                                           attributes: [.foregroundColor: NSColor.labelColor])
     string.addAttribute(.link, value: About.homepageURL, range: NSRange(location: 0, length: 7))
     string.addAttribute(.link, value: About.githubURL, range: NSRange(location: 8, length: 6))
     string.addAttribute(.link, value: About.supportEmailURL, range: NSRange(location: 15, length: 7))
     return string
   }
-
+  
+  private var shortSpacingLine: NSAttributedString {
+    let spacingStyle = NSMutableParagraphStyle()
+    spacingStyle.maximumLineHeight = 6
+    return NSAttributedString(string: "\n", attributes: [.paragraphStyle: spacingStyle])
+  }
+  
   private var credits: NSAttributedString {
-    let credits = NSMutableAttributedString(string: "",
-                                            attributes: [NSAttributedString.Key.foregroundColor: NSColor.labelColor])
+    let credits = NSMutableAttributedString(string: "", attributes: [.foregroundColor: NSColor.labelColor])
     credits.append(links)
-    credits.append(NSAttributedString(string: "\n\n"))
+    credits.append(NSAttributedString(string: "\n"))
+    credits.append(shortSpacingLine)
     credits.append(blurb)
     credits.append(NSAttributedString(string: "\n"))
     credits.append(introLink)
-    credits.append(NSAttributedString(string: "\n\n"))
+    credits.append(NSAttributedString(string: "\n"))
+    credits.append(shortSpacingLine)
     credits.append(forkCredits)
     credits.setAlignment(.center, range: NSRange(location: 0, length: credits.length))
     return credits
   }
-
+  
+  private var version: String {
+    let infoPlistVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "1.0"
+    #if FOR_APP_STORE
+    return infoPlistVersion + " for Mac App Store"
+    #else
+    return infoPlistVersion + " non-App Store build"
+    #endif
+  }
+  
   @objc
   func openAbout() {
     NSApp.activate(ignoringOtherApps: true)
-    NSApp.orderFrontStandardAboutPanel(options: [NSApplication.AboutPanelOptionKey.credits: credits])
+    NSApp.orderFrontStandardAboutPanel(options: [.credits: credits, .applicationVersion: version])
   }
 }
